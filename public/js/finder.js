d5e8c52060f26428fb70e8044972ea60b3fdf0dc
twicetheprogrammer <njabulozweli08@gmail.com>
2017-12-14 20:05:19 +0200

    Second semi final commit

--- public/js/finder.js
+++ public/js/finder.js
@@ -1,43 +1,49 @@
-$(document).ready(function(){
-        //======================= CAPETOWN TEASER SLIDER =================
-          // 
-          // var myIndex = 0;
-          // carousel();
-          //
-          // function carousel() {
-          //     var i;
-          //     var x = document.getElementsByClassName("slide-show");
-          //     for (i = 0; i < x.length; i++) {
-          //        x[i].style.display = "none";
-          //     }
-          //     myIndex++;
-          //     if (myIndex > x.length) {myIndex = 1}
-          //     x[myIndex-1].style.display = "block";
-          //     setTimeout(carousel, 2000);
-          // }
-
-
+$(document).ready(() => {
         //=============== DISPLAY ALL ACTIVITIES =================
-        (function(){
+        (() => {
+              let all_activities = [] , btn_id = 0;
               $.ajax({
                       dataType : 'json',
                       url : 'http://127.0.0.1:8081/all_activities',
-                      success : function(activities){
-
+                      success : (activities) => {
                               for (let activity of activities){
-                                  $('.activity').append(
-                                              '<h3 class="display-6" align="center">'+activity.name+'</h3>'
+                                  all_activities.push(activity.name);
+                                  $('.activity-container').append(
+                                        '<fieldset class="activity">'
+                                              +'<h3 align="center">'+activity.name+'</h3>'
                                               +'<hr>'
-                                              +'<p name="description">'+activity.description+'</p>'
-                                              +'<ul>'
-                                                    +'<li name="location">Location : '+activity.location+'</li>'
-                                                    +'<li name="duration">Duration : '+activity.duration+' hours</li>'
-                                                    +'<li name="cost">Cost : R '+activity.cost+'</li>'
-                                              +'</ul><hr>'
-                                              +'<p name="rating">Rating&nbsp;    <span class="fa fa-star checked" style="font-size: 15px;color : orange;"></span><p>'
-                                              +'<input type="submit" value="Book"  style="float:right;border-radius: 15px; width:50%; background-color: #f1f1f1;"><br><br>'
-                                  );
+                                              +'<img src="'+activity.image+'width="" height="200px">'
+                                              +'<hr><br>'
+                                              +'&nbsp; Reviews ('+activity.rating.length+') <button type="button" data-btn-id="'+(btn_id += 1)+'"name="button" class="rating">More&nbsp;<i class="fa fa-caret-down"></i></button>'
+                                        +'</fieldset>'
+                                 );
                               }
+
+                              //================== FILTERING BY CATERGORY ================
+
+                              var $radios = $('input[type="radio"]');
+                              $radios.change(() => {
+                                      $filtered =  $radios.filter(':checked').val();
+                                      if ( all_activities.includes($filtered)){
+                                            $('.activity-container').html('');
+                                            $('.activity-container').append(
+                                                  '<fieldset class="activity">'
+                                                        +'<h3 align="center">'+$filtered+'</h3>'
+                                                        +'<hr>'
+                                                         +'<img src="'+activity.image+'width="" height="200px">'
+                                                         +'<hr><br>'
+                                                         +'&nbsp; Reviews ('+activity.rating.length+')'
+                                                  +'</fieldset>'
+                                           );
+                                      }
+                              });
+
+
+                        // ================= DISPLAY RATING DETAILS ====================
+
+                            $('.rating').on('click' , ()=>{
+                                      
+                            });
                       }
               });
         })();
--- server.js
+++ server.js
@@ -15,16 +15,14 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(express.static('public'));
 
-
-
 // ====================== GET HOME PAGE =====================
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
         res.sendFile(__dirname + '/' +'index.html');
 })
 //===================== CREATE ACTIVITY ================
 
-app.post('/activity', urlencodedParser, function (req, res) {
+app.post('/activity', urlencodedParser, (req, res) => {
 
       var newActivity = new Activity({
           name:req.body.name,
@@ -35,7 +33,7 @@ app.post('/activity', urlencodedParser, function (req, res) {
       console.log(newActivity);
       res.end(JSON.stringify(newActivity));
 
-        newActivity.save(function(err) {
+        newActivity.save((err) => {
         if (err) throw err;
 
         console.log('Activity saved successfully!');
@@ -44,51 +42,20 @@ app.post('/activity', urlencodedParser, function (req, res) {
 
 //===================== GET ALL ACTIVITIES ===============
 
-app.get('/all_activities' , function(req , res){
-  Activity.find({}, function(err, activities) {
+app.get('/all_activities' , (req , res) => {
+  Activity.find({},(err, activities) => {
      if (err) throw err;
      res.end(JSON.stringify(activities));
    });
 })
 
-// //=============== UPDATE QUOTE ===================
-//
-// app.post('/update', urlencodedParser, function (req, res) {
-//
-//     var updateAcitvity = {
-//         name:req.body.name,
-//         cost: req.body.cost,
-//         location: req.body.location,
-//         duration : req.body.duration
-//   };
-//   //var_id = mongoose.Types.ObjectId(req.body.id);
-//           Activity.findByIdAndUpdate( quote_id , updateQuote ,  function(err , quote){
-//                 if(err){
-//                       throw err  ;
-//                 }
-//                 console.log('Quote updated successfully!');
-//           });
-//           res.redirect('/')
-// })
-//================== DELETE QUOTE ====================
+//======================= SORT WITH CATERGORY ================
 
-// app.post('/delete' , urlencodedParser ,  function( req , res){
-//
-//         var quote_id = mongoose.Types.ObjectId(req.body.id);
-//         Quote.findById(quote_id , function(err , quote){
-//                   if(err) throw err ;
-//
-//                   quote.remove(function(err){
-//                       if ( err) {
-//                           throw err;
-//                       }
-//                       console.log('Quote deleted successfully!');
-//                   })
-//         });
-//         res.redirect('/')
-// });
+app.post('/catergory' , urlencodedParser ,  (req , res) => {
+          var catergory = req.body.value ;
+})
 // =================== EVENT LISTENER =============================
-var server = app.listen(8081, function () {
+var server = app.listen(8081,() => {
    var host = server.address().address
    var port = server.address().port
 
