dd5f6ceab67f96a39a25312f967d07f5f8f5121e
twicetheprogrammer <njabulozweli08@gmail.com>
2017-12-15 15:50:08 +0200

    Unresponsive activity finder

--- public/js/finder.js
+++ public/js/finder.js
@@ -12,11 +12,12 @@ $(document).ready(() => {
                                         '<fieldset class="activity">'
                                               +'<h3 align="center">'+activity.name+'</h3>'
                                               +'<hr>'
-                                              +'<img src="'+activity.image+'width="" height="200px">'
+                                              +'<img src="'+activity.image+'width="" height="200px" alt="Activity image">'
                                               +'<hr><br>'
                                               +'&nbsp; Reviews ('+activity.rating.length+') <button type="button" data-btn-id="'+(btn_id += 1)+'"name="button" class="rating">More&nbsp;<i class="fa fa-caret-down"></i></button>'
                                         +'</fieldset>'
                                  );
+                                 $('#activities').append('<option value="'+activity.name+'"></option>');
                               }
 
                               //================== FILTERING BY CATERGORY ================
@@ -24,25 +25,14 @@ $(document).ready(() => {
                               var $radios = $('input[type="radio"]');
                               $radios.change(() => {
                                       $filtered =  $radios.filter(':checked').val();
-                                      if ( all_activities.includes($filtered)){
-                                            $('.activity-container').html('');
-                                            $('.activity-container').append(
-                                                  '<fieldset class="activity">'
-                                                        +'<h3 align="center">'+$filtered+'</h3>'
-                                                        +'<hr>'
-                                                         +'<img src="'+activity.image+'width="" height="200px">'
-                                                         +'<hr><br>'
-                                                         +'&nbsp; Reviews ('+activity.rating.length+')'
-                                                  +'</fieldset>'
-                                           );
-                                      }
+                                      $('input[value~="Canal Cruise"]').parent().hide();
+                                      console.log($filtered);
                               });
-
 
                         // ================= DISPLAY RATING DETAILS ====================
 
                             $('.rating').on('click' , ()=>{
-                                      
+
                             });
                       }
               });
